1aef9907634a862f129d9313747b58506aafe952
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-08-07 14:53:42 -0300

    Added more functionalities for the Game Controller

--- src/main/java/pb/art5019/GameController.java
+++ src/main/java/pb/art5019/GameController.java
@@ -5,24 +5,60 @@ import java.util.Random;
 
 public class GameController {
     private int[] usedTips;
-    private Player[] players;
     private int guesserId;
     private int remainingTime;
     private ArrayList<Integer> usedCardsIds = new ArrayList<>();
+    private ArrayList<Player> players = new ArrayList<>();
     private int matchNumber = 1;
     private boolean isRunning = true;
     private ArrayList<Card> cards = new ArrayList<>();
     private Card currentCard;
     private int stackPosition;
     private Random randomizer = new Random();
+    private int currentPrize = 0;
+    private int maxPrize = 0;
+    private int currentPlayerId;
 
     public void gameStartPoint() {
+
         shuffleCards();
+        shufflePlayers();
         pickACard();
 
+        maxPrize = currentCard.tips.length;
+        currentPrize = maxPrize;
 
 
+        gameLoop();
 
+    }
+
+    public void gameLoop() {
+
+    }
+
+    public void makeAGuess(String guess) {
+        if(guess.toLowerCase().equals(currentCard.name.toLowerCase())) {
+           defaultPonctuation();
+           return;
+        }
+
+    }
+
+    private void rotate() {
+        currentPlayerId++;
+        if(currentPlayerId == getConnectedPlayers()) {
+            currentPlayerId = 0;
+        }
+    }
+
+
+    private void defaultPonctuation() {
+        ponctuate(currentPrize, currentPlayer());
+    }
+
+    private void ponctuate(int points, Player player) {
+        player.setPoints(player.getPoints() + points);
     }
 
     private void shuffleCards() {
@@ -30,15 +66,24 @@ public class GameController {
         cards = new ArrayList<>(cards.stream().sorted().toList());
     }
 
+    private Player currentPlayer() {
+        return players.get(currentPlayerId);
+    }
+
 
     private void pickACard() {
+        stackPosition++;
         currentCard = cards.get(stackPosition);
     }
 
     private void shufflePlayers() {
-
+        players.forEach(players -> players.setRandomizedOrder(randomizer.nextInt()));
+        players = new ArrayList<>(players.stream().sorted().toList());
     }
 
+    private int getConnectedPlayers() {
+        return players.size();
+    }
 
     public int[] getUsedTips() {
         return usedTips;
--- src/main/java/pb/art5019/Player.java
+++ src/main/java/pb/art5019/Player.java
@@ -2,16 +2,27 @@ package pb.art5019;
 
 public class Player {
     private String name;
+
+
+
     private int points;
-    private int order;
+    private int randomizedOrder;
     private boolean isBlocked;
 
-    public void setOrder(int order) {
-        this.order = order;
+    public void setRandomizedOrder(int randomizedOrder) {
+        this.randomizedOrder = randomizedOrder;
     }
 
-    public int getOrder() {
-        return order;
+    public int getRandomizedOrder() {
+        return randomizedOrder;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
     }
 
 }
