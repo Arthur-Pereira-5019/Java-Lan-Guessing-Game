030be91cea59a4292f56c6c7a67526c9adfad3fa
Arthur <peneir20@gmail.com>
2025-08-14 15:04:59 -0300

    Added more functionalities to the Game Manager, started to work with conections using java Socket class

---
+++ src/main/java/pb/art5019/Client.java
@@ -0,0 +1,9 @@
+package pb.art5019;
+
+public class Client {
+
+    public void setupClient() {
+
+    }
+
+}
--- src/main/java/pb/art5019/GameController.java
+++ src/main/java/pb/art5019/GameController.java
@@ -1,48 +1,113 @@
 package pb.art5019;
 
+import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 
 public class GameController {
-    private int[] usedTips;
-    private int guesserId;
-    private int remainingTime;
-    private ArrayList<Integer> usedCardsIds = new ArrayList<>();
-    private ArrayList<Player> players = new ArrayList<>();
-    private int matchNumber = 1;
+    //Managing variables
     private boolean isRunning = true;
-    private ArrayList<Card> cards = new ArrayList<>();
-    private Card currentCard;
-    private int stackPosition;
     private Random randomizer = new Random();
-    private int currentPrize = 0;
-    private int maxPrize = 0;
-    private int currentPlayerId;
+    private int matchNumber = 1;
+    private int remainingTime;
 
-    public void gameStartPoint() {
+    //Prize related variables
+    private int maxPrize = 0;
+    private int currentPrize = 0;
+
+    //Tips related variables
+    private int[] usedTips;
+
+    //Player related variables
+    private int currentPlayerId;
+    private ArrayList<Player> players = new ArrayList<>();
+
+    //Cards related variables
+    private int stackPosition;
+    private Card currentCard;
+    private ArrayList<Card> cards = new ArrayList<>();
+
+    //Conection methods
+    Socket socket;
+    InputStream input;
+    OutputStream output;
+
+
+    public void gameStartPoint() throws IOException {
+        startConnection();
+        System.out.println("Connected at IP: " + socket.getLocalAddress() + "and port " + socket.getPort());
+        solveCards();
 
         shuffleCards();
         shufflePlayers();
-        pickACard();
-
         maxPrize = currentCard.tips.length;
         currentPrize = maxPrize;
 
-
         gameLoop();
+    }
 
+    public void solveCards() throws IOException {
+        File sourceFolder = new File("target");
+        ArrayList<File> JSONFiles = new ArrayList<>();
+        ArrayList<Card> Cards = new ArrayList<>();
+        JSON jsonManipulator = new JSON();
+        FilenameFilter acceptJSON = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.contains(".json");
+            }
+        };
+
+        JSONFiles.addAll(List.of(sourceFolder.listFiles(acceptJSON)));
+        for(File thisFile: JSONFiles) {
+            Cards.add(jsonManipulator.readValue(thisFile, Card.class));
+        }
+
+        for(Card card: Cards) {
+            System.out.println(card.name);
+        }
+    }
+
+    public void startConnection() throws java.io.IOException {
+        socket = new Socket("127.0.0.1", 5019);
+        input = socket.getInputStream();
+        output = socket.getOutputStream();
+    }
+
+    public String getIp() {
+        try(final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            return socket.getLocalAddress().getHostAddress();
+        } catch (SocketException | UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void gameLoop() {
+        while(isRunning) {
+            pickACard();
+            if(getConnectedPlayers() == 0 || stackPosition == cards.size()) {
+                isRunning = false;
+            }
+        }
+        endGame();
+    }
 
+    private void endGame() {
+        matchNumber++;
     }
 
     public void makeAGuess(String guess) {
-        if(guess.toLowerCase().equals(currentCard.name.toLowerCase())) {
-           defaultPonctuation();
+        if(guess.equalsIgnoreCase(currentCard.name)) {
+           rightGuess();
            return;
         }
-
+        wrongGuess();
     }
 
     private void rotate() {
@@ -50,6 +115,25 @@ public class GameController {
         if(currentPlayerId == getConnectedPlayers()) {
             currentPlayerId = 0;
         }
+    }
+
+    private void rightGuess() {
+        defaultPonctuation();
+        rotate();
+    }
+
+    private void wrongGuess() {
+        rotate();
+        nextPrize();
+    }
+
+
+    private void nextPrize() {
+        currentPrize--;
+        if(currentPrize==0) {
+
+        }
+
     }
 
 
@@ -91,22 +175,6 @@ public class GameController {
 
     public void setUsedTips(int[] usedTips) {
         this.usedTips = usedTips;
-    }
-
-    public Player[] getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(Player[] players) {
-        this.players = players;
-    }
-
-    public int getGuesserId() {
-        return guesserId;
-    }
-
-    public void setGuesserId(int guesserId) {
-        this.guesserId = guesserId;
     }
 
     public int getRemainingTime() {
